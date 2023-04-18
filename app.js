150798d52953a98e42d680fd313ae01be9d432aa
geniusjoelraj <geniusjoel2005@gmail.com>
2023-04-18 20:04:37 +0530

    Added port for render

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require("express");
 const https = require("https");
 const bodyParser = require("body-parser");
 const app = express();
+const port = process.env.PORT || 3000;
 
 app.use(bodyParser.urlencoded({extended:true}));
 
@@ -53,7 +54,7 @@ app.post("/weather", function(req, res) {
 
 
 
-app.listen(3000, function() {
+app.listen(port, function() {
   console.log("The server is running on port 3000");
 });
 
